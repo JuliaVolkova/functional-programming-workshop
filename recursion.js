4ea24b32a2ab1a0956a33b9c4cfcd29896a93615
Julia Volkova <julia.volkova789@gmail.com>
2018-02-26 21:32:28 +0300

    Implement recursion without mutation

--- recursion.js
+++ recursion.js
@@ -1,14 +1,14 @@
 const sum = (prev, next) => prev + next;
 
-const myArray = [1,2,3];
+const myArray = [1, 2, 3, 4, 5];
 
-function reduce(arr, fn, initial) {
-    if(arr.length === 0) return initial;
-    let result = fn(initial, arr.shift());
-    return reduce(arr, fn, result);
+function reduce(arr, fn, initial, index = 0) {
+    if(index === arr.length) return initial;
+    const result = fn(initial, arr[index]);
+
+    return reduce(arr, fn, result, index + 1);
 }
 
 reduce(myArray, sum, 0);
 
 module.exports = reduce;
-
