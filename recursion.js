ce15efd9955862f5b4eba0a03c68aaf39dc50618
Julia Volkova <julia.volkova789@gmail.com>
2018-02-26 21:32:28 +0300

    Implement recursion without mutation, add JSDoc

--- recursion.js
+++ recursion.js
@@ -1,14 +1,38 @@
+/**
+ * @param {*} prev
+ * @param {*} next
+ * @return {*} Returns sum of passed values
+ */
 const sum = (prev, next) => prev + next;
 
-const myArray = [1,2,3];
-
-function reduce(arr, fn, initial) {
-    if(arr.length === 0) return initial;
-    let result = fn(initial, arr.shift());
-    return reduce(arr, fn, result);
+/**
+ * @typedef {*} T Type of elements of the {@code array}.
+ * @typedef {*} R Type of reduction result.
+ *
+ * @callback reducer
+ * @param {R} result
+ * @param {T} next
+ * @param {number} index
+ * @param {Array.<T>} array
+ * @returns {R}
+ */
+/**
+ * Function accumulates values using recursion.
+ *
+ * @example
+ * const someArray = [1, 2, 3, 4, 5];
+ * const reduceValue = reduce(someArray, sum, 0); // 15
+ *
+ * @param {Array.<T>} arr
+ * @param {reducer} fn
+ * @param {R} initial
+ * @param {number} [index = 0]
+ * @returns {R} - Returns reduced value
+ */
+function reduce(arr, fn, initial, index = 0) {
+    if(index === arr.length) return initial;
+    const result = fn(initial, arr[index], index, arr);
+    return reduce(arr, fn, result, index + 1);
 }
 
-reduce(myArray, sum, 0);
-
 module.exports = reduce;
-
